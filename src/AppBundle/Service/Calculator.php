35c7ab7a9d4cae2f4717ca34032b2c3fea781115
ChristopheBDM <elninodevegas@gmail.com>
2017-08-26 16:59:28 +0200

    - ajout de la méthode isNotWorkable pour gérer le cas de reza les jours fériés - modification de la méthode random

--- src/AppBundle/Service/Calculator.php
+++ src/AppBundle/Service/Calculator.php
@@ -24,6 +24,7 @@ class Calculator
     public function __construct(array $listeTarifs)
     {
         $this->listeTarifs = $listeTarifs;
+
     }
 
     public function ageCalculatorFromToday(\DateTime $dateInitiale)
@@ -51,6 +52,61 @@ class Calculator
         return $dateReza < $dateNow;
     }
 
+    function isNotWorkable(\DateTime $date)
+    {
+        $year = date('Y',$date->getTimestamp());
+
+        $easterDate  = easter_date($year);
+        $easterDay   = date('j', $easterDate);
+        $easterMonth = date('n', $easterDate);
+        $easterYear   = date('Y', $easterDate);
+
+        $holidays = array(
+            // Dates fixes
+            mktime(0, 0, 0, 1,  1,  $year),  // 1er janvier
+            mktime(0, 0, 0, 5,  1,  $year),  // Fête du travail
+            mktime(0, 0, 0, 5,  8,  $year),  // Victoire des alliés
+            mktime(0, 0, 0, 7,  14, $year),  // Fête nationale
+            mktime(0, 0, 0, 8,  15, $year),  // Assomption
+            mktime(0, 0, 0, 11, 1,  $year),  // Toussaint
+            mktime(0, 0, 0, 11, 11, $year),  // Armistice
+            mktime(0, 0, 0, 12, 25, $year),  // Noel
+
+            // Dates variables
+            mktime(0, 0, 0, $easterMonth, $easterDay + 1,  $easterYear),
+            mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear),
+            mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear),
+        );
+
+        return in_array($date->getTimestamp(), $holidays);
+    }
+
+    public function overSellForADay($repositoryCommande, $repositoryBillet, $commande)
+    {
+        $listeId = [];
+
+        $commandesJour = $repositoryCommande->findBy(array(
+                'datereza' => $commande->getDatereza())
+        );
+
+        foreach ($commandesJour as $commandeJour) {
+            $listeId[] = $commandeJour->getId();
+        }
+
+        if ($listeId != null) {
+            $billetsJour = $repositoryBillet->findBy(array(
+                'commande' => $listeId
+            ));
+            dump($billetsJour);
+
+            echo count($billetsJour);
+
+            if (count($billetsJour) > 1000) {
+                return true;
+            }
+        }
+    }
+
     public function priceCalculator(Commande $commande)
     {
         foreach ($commande->getBillets() as $billet)
@@ -69,13 +125,10 @@ class Calculator
 
     public function random()
     {
-        $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        $string = '';
-        $max = strlen($characters - 1);
+        $string = substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 1).substr(md5(time()),1);
 
-        for ($i = 0; $i <= 10; $i++) {
-            $string .= $characters[mt_rand(0, $max)];
-        }
         return $string;
+
+
     }
 }
