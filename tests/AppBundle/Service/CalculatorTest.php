3a78ffa4be4846a15d8c5845ffd9c209835cf0d0
ChristopheBDM <elninodevegas@gmail.com>
2017-08-28 14:02:39 +0200

    - création d'une classe de test

---
+++ tests/AppBundle/Service/CalculatorTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 27/08/2017
+ * Time: 21:19
+ */
+
+namespace tests\AppBundle\Service;
+
+use Symfony\Bundle\WebProfilerBundle\Tests\TestCase;
+
+class CalculatorTest extends TestCase
+{
+
+}
